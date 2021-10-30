e902db32a9c4249f3576a0229eb22b45f23f23fb
Marta Molina <marta.molina@live.u-tad.com>
2021-10-30 23:48:34 +0200

    Temporary item view. Looking for components on the materialDesign site

---
+++ JsonMaterialDesign/app/src/main/java/com/marta/jsonmaterialdesign/model/Films.kt
@@ -0,0 +1,8 @@
+package com.marta.jsonmaterialdesign.model
+
+import com.google.gson.annotations.SerializedName
+
+data class FilmsResponse (
+    @SerializedName("films")
+    val films: MutableList<Film>
+)
